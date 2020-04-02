e6db0124dc88fddd287691bddca62bbc068d2532
Pablo Gómez <pablogomez@pablogomez.com>
2020-04-02 18:29:52 +0100

    -urxvt

--- 110-install_i3.sh
+++ 110-install_i3.sh
@@ -4,4 +4,4 @@ set -e
 echo "----------------------------------------------------------------------"
 echo "     Installing i3"
 echo "----------------------------------------------------------------------"
-yay -S --needed --noconfirm i3-gaps rxvt-unicode dmenu rxvt-unicode-terminfo i3status i3lock picom
+yay -S --needed --noconfirm i3-gaps dmenu i3status i3lock picom termite
