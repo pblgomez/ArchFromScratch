837bb66af5c15b864977ee9b7cd1b67b544c1ce6
Pablo Gómez <pablogomez@pablogomez.com>
2020-04-08 17:31:12 +0100

    +gnu-free-fonts

--- 100-install_X.sh
+++ 100-install_X.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Installing packages
-yay -Syu --needed --noconfirm xorg-server xf86-video-intel xorg-xinit
+yay -Syu --needed --noconfirm xorg-server xf86-video-intel xorg-xinit gnu-free-fonts
 
 
 echo "----------------------------------------------------------------------"
