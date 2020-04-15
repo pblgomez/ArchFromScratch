908b8d590407eb09ae492e0a9aad4e4e18c7e358
Pablo Gómez <pablogomez@pablogomez.com>
2020-04-15 20:13:25 +0100

    +xrandr

--- 100-install_X.sh
+++ 100-install_X.sh
@@ -3,7 +3,7 @@ set -e
 
 # Installing packages
 yay -Syu --needed --noconfirm xorg-server xf86-video-intel xorg-xinit \
-	gnu-free-fonts xrandr
+	gnu-free-fonts xorg-xrandr
 
 
 echo "----------------------------------------------------------------------"
